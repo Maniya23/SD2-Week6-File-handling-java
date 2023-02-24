7ce632b1710d0540a601105176bc924d57f448e0
Manishka Fonseka <manishkafon@gmail.com>
2023-02-25 00:14:33 +0530

    Read and write files

--- src/FileWrite.java
+++ src/FileWrite.java
@@ -1,5 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWrite {
     public static void main(String[] args) {
-        
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("This is what i want to be written");
+            myWriter.close();
+            System.out.println("Successfully wrote to file");
+
+        } catch (IOException e) {
+            System.out.println("An error has occurred while trying to write file");
+        }
     }
 }
